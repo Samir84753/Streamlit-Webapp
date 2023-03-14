f4763acc1c0fd330e62bb7d8690f6fa6b7f0bb65
samir.maharjan <samir84753@gmail.com>
2023-03-14 14:33:30 +0545

    added files

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Saved Blog Manager",
+    page_icon="📖",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Report a bug': "https://github.com/Samir84753/EcommerceWeb-project",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    })
+st.title("APP")
