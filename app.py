929f06b8ab16a8e208ded41b2d4d75c9d3f56e85
samir.maharjan <samir84753@gmail.com>
2023-03-27 10:38:54 +0545

    resume view and download

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ import streamlit as st
 st.set_page_config(
     page_title="Saved Blog Manager",
     page_icon="📖",
-    layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Report a bug': "https://github.com/Samir84753/Streamlit-Webapp",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    })
+        "Report a bug": "https://github.com/Samir84753/Streamlit-Webapp/issues",
+        "About": "This is a web application made with Streamlit.",
+    },
+)
 st.title("APP")
+st.markdown(
+    """
+        **Github Repo:** https://github.com/Samir84753/Streamlit-Webapp
+    """
+)
