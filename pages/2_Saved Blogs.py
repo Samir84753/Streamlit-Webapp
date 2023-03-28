54e6fb935e8fae063e2c8020c227da49164c260a
samir.maharjan <samir84753@gmail.com>
2023-03-28 07:51:26 +0545

    streamlit-auth

--- pages/2_Saved Blogs.py
+++ pages/2_Saved Blogs.py
@@ -1,19 +1,26 @@
 import streamlit as st
 import json
-from dotenv import load_dotenv
-load_dotenv()
 import os
-import pyperclip
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Define the filename for the JSON file
 json_file = "blog_urls.json"
 
+
 def download_json_file():
-    with open(json_file, 'r') as f:
+    with open(json_file, "r") as f:
         data = json.load(f)
-    with open("downloaded.json", 'w') as f:
+    with open("downloaded.json", "w") as f:
         json.dump(data, f)
     st.sidebar.success(f"{json_file} downloaded successfully as a JSON file!")
+
 
 # Load the blog URLs from the JSON file
 try:
@@ -22,9 +29,22 @@ try:
 except FileNotFoundError:
     blog_dict = {}
 
-# Define the password
+# Define the .env variables
 
-password = os.environ.get('PASSWORD')
+password = os.environ.get("PASSWORD")
+cookie_key = os.environ.get("COOKIE_KEY")
+cookie_name = os.environ.get("COOKIE_NAME")
+
+# login widget
+with open("config.yaml") as file:
+    config = yaml.safe_load(file)
+authenticator = stauth.Authenticate(
+    config["credentials"],
+    cookie_name,
+    cookie_key,
+    config["cookie"]["expiry_days"],
+)
+
 
 # Define a function to check the user's password
 def user_auth():
@@ -33,46 +53,57 @@ def user_auth():
     Returns:
         bool: True if password matches. Else False.
     """
-    user_password = st.sidebar.text_input("Enter the password to add or delete blog URLs", type="password")
-    if user_password == password:
+    authenticator.login("Login", "sidebar")
+    if "key" not in st.session_state:
+        st.session_state.setdefault("key", cookie_key)
+
+    if st.session_state["authentication_status"]:
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        authenticator.logout("Logout", "sidebar")
         return True
-    elif user_password != "":
-        st.sidebar.warning("Incorrect password")
-    return False
+    elif st.session_state["authentication_status"] == False:
+        st.error("Username/password is incorrect")
+    elif st.session_state["authentication_status"] == None:
+        st.warning("Please enter your username and password")
+
 
 # Define a function to save the blog URLs to the JSON file
 def save_to_json():
-    """Saves content to json file
-    """
+    """Saves content to json file"""
     with open(json_file, "w") as f:
         json.dump(blog_dict, f)
 
+
 def add_blog():
-    """Saves the user input of title,url, and section to json file.
-    """
+    """Saves the user input of title,url, and section to json file."""
     st.sidebar.header("Add a Blog URL")
-    blog_title= st.sidebar.text_input("Input Blog Title")
-    blog_url= st.sidebar.text_input("Input Blog URL")
-    blog_section = st.sidebar.selectbox("Select a section",list(blog_dict.keys()))
+    blog_title = st.sidebar.text_input("Input Blog Title")
+    blog_url = st.sidebar.text_input("Input Blog URL")
+    blog_section = st.sidebar.selectbox("Select a section", list(blog_dict.keys()))
 
     if st.sidebar.button("Save"):
         blog_dict.setdefault(blog_section, {})[blog_title] = blog_url
         st.sidebar.success("Saved")
         save_to_json()
 
+
 def delete_blog():
-    """Deletes blog url from json file.
-    """
+    """Deletes blog url from json file."""
     st.sidebar.header("Delete Blog")
-    blog_section = st.sidebar.selectbox("Select a section for the blog URL to delete", list(blog_dict.keys()))
-    blog_title = st.sidebar.selectbox("Select the blog URL to delete", list(blog_dict[blog_section].keys()))
+    blog_section = st.sidebar.selectbox(
+        "Select a section for the blog URL to delete", list(blog_dict.keys())
+    )
+    blog_title = st.sidebar.selectbox(
+        "Select the blog URL to delete", list(blog_dict[blog_section].keys())
+    )
     if st.sidebar.button("Delete"):
         del blog_dict[blog_section][blog_title]
         st.sidebar.success(f"Deleted")
         save_to_json()
+
+
 def add_section():
-    """Creates New section.
-    """
+    """Creates New section."""
     st.sidebar.header("Create a New Section")
     new_section = st.sidebar.text_input("Enter a name for the new section")
     if st.sidebar.button("Create"):
@@ -82,15 +113,17 @@ def add_section():
             blog_dict[new_section] = {}
             st.sidebar.success(f"Created '{new_section}' section")
             save_to_json()
+
+
 def remove_section():
-    """Deletes section from json.
-    """
+    """Deletes section from json."""
     st.sidebar.header("Delete Section")
-    blog_section=st.sidebar.selectbox("select section",list(blog_dict.keys()))
+    blog_section = st.sidebar.selectbox("select section", list(blog_dict.keys()))
     if st.sidebar.button("Delete Section"):
         del blog_dict[blog_section]
         st.sidebar.success(f"Deleted")
         save_to_json()
+
 
 def search():
     """Searches for content on json file.
@@ -107,9 +140,13 @@ def search():
                 results.append(f"- [{url_name}]({url_link})")
         else:
             for url_name, url_link in urls.items():
-                if search_term.lower() in url_name.lower() or search_term.lower() in url_link.lower():
+                if (
+                    search_term.lower() in url_name.lower()
+                    or search_term.lower() in url_link.lower()
+                ):
                     results.append(f"- {section}: [{url_name}]({url_link})")
     return results
+
 
 def main():
     st.title("# Saved Blog Manager")
@@ -121,10 +158,9 @@ def main():
         remove_section()
     else:
         st.header("## Authenticate to add urls")
-    if st.sidebar.button('Download JSON file'):
-        
+    if st.sidebar.button("Download JSON file"):
         download_json_file()
-    #view section
+    # view section
     results = search()
     if results:
         for result in results:
@@ -135,5 +171,6 @@ def main():
             for url_name, url_link in urls.items():
                 st.write(f"- [{url_name}]({url_link})")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
