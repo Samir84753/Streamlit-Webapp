0f20ba13ea3c9d42a25acac9e6b670ca9fa79ddf
samir.maharjan <samir84753@gmail.com>
2023-03-28 08:09:31 +0545

    resume page

--- pages/1_Resume.py
+++ pages/1_Resume.py
@@ -1,19 +1,3 @@
 import streamlit as st
 
-# load html and pdf file.
-with open("assets/Samir_resume.html", "r") as f:
-    html_string = f.read()
-pdf_path = "assets/Samir_resume.pdf"
-with open(pdf_path, "rb") as f:
-    pdf_bytes = f.read()
-
-# html display
-st.components.v1.html(html_string, width=950, height=1210)
-
-# download button for resume on sidebar.
-st.sidebar.download_button(
-    "Download Resume",
-    data=pdf_bytes,
-    file_name="Samir_resume.pdf",
-    mime="application/pdf",
-)
+st.write("Work in Progress.")
